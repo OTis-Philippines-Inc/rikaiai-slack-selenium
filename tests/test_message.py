3b5b32490d82dde627e34a115c9d513369d9369d
Isiah Jordan Dimaunahan <dimaunahanisiahjordan@gmail.com>
2025-03-28 19:25:30 +0800

    fix channel option

--- tests/test_message.py
+++ tests/test_message.py
@@ -18,11 +18,12 @@ class MessagePage(BaseCase):
         
         # Delete Message
         index: int = 0
-        for message_index in [0, 2]:
-            # Index is to make sure that after deletion the message_index
-            # will consider the changes on the list of user message
-            Message().delete_message(self, USER, message_index-index, CHANNEL)
-            index+=1
+        for channel in CHANNEL:
+            for message_index in [0, 2]:
+                # Index is to make sure that after deletion the message_index
+                # will consider the changes on the list of user message
+                Message().delete_message(self, USER, message_index-index, channel)
+                index+=1
 
         #import pdb; pdb.set_trace()
 
