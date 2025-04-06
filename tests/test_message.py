1ad0522f68edabfb33d7d392b175043203ee12f2
Isiah Jordan Dimaunahan <dimaunahanisiahjordan@gmail.com>
2025-04-07 06:14:06 +0800

    feat: delete and verify billing change

--- tests/test_message.py
+++ tests/test_message.py
@@ -5,25 +5,41 @@ import os, sys
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "actions", "messagePage")))
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "actions", "loginPage")))
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "config")))
-from settings import USER, EMAIL, SITE, CHANNEL
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "utilities")))
+from settings import USER, EMAIL, SITE, CHANNEL, MSG_INPUT
 from login import Login
 from message import Message
+from billing import Billing
+from playback import Playback
 
 BaseCase.main(__name__, __file__)
 
 class MessagePage(BaseCase):
+    def setUp(self):
+        super().setUp()
+        self.record = Playback(self.driver)
+
     def test_message_delete(self) -> None:
+        self.record.start_recording("test_message_delete")
         Login().login_to_page(self, EMAIL, SITE)
         self.wait(5)
         
         # Delete Message
-        index: int = 0
+        msg_handle: object = Message(self)
+        bill_handle: object = Billing(self, msg_handle)
         for channel in CHANNEL:
-            for message_index in [0, 2]:
-                # Index is to make sure that after deletion the message_index
-                # will consider the changes on the list of user message
-                Message().delete_message(self, USER, message_index-index, channel)
-                index+=1
+            # Fetch current bill score for channel
+            bill_handle.update_bill() # Update current bll score
+            points: float = bill_handle.get_channel_bill(channel) # Monitor difference in score
+            for index, text, translation in MSG_INPUT:
+                msg_handle.delete_message(USER, index, channel)
+            
+            bill_handle.update_bill()
+            self.assert_equal(points, bill_handle.get_channel_bill(channel))
 
-        #import pdb; pdb.set_trace()
+        # Closing the recording
+        self.record.stop_and_save_recording()
+        self.driver.close()
+
+
 
