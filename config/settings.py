f8321378e80faf378cda66b5a6dc51e755a11168
Isiah Jordan Dimaunahan <dimaunahanisiahjordan@gmail.com>
2025-04-07 05:04:33 +0800

    edit: new settings

--- config/settings.py
+++ config/settings.py
@@ -1,6 +1,5 @@
 from dotenv import dotenv_values
 from datetime import datetime, timezone
-import numpy as np
 import os 
 
 # Google API 
@@ -10,29 +9,29 @@ SECRET_NAME: str = "client_secret.json"
 TRIM_CONTENT: int = 200
 
 # Several options that will be used for testing
-STRIP_OPT: object = np.array([
+STRIP_OPT: list = [
         "full", # Full match comparison
         "left", # Prefix match comparison
         "right" # Suffix match comparison
-    ])
+    ]
 
-SORT_OPT: object = np.array([
+SORT_OPT: list = [
         "None", # Don`t sort
         "Asc", # Ascending order option
         "Desc" # Descending order option
-    ])
+    ]
 
 # What would be used to test
-CHANNEL: object = np.array([
+CHANNEL: list = [
         "rikaiai-features",
         "social"
-    ])
+    ]
 
 # Text that will be used for testing
-MSG_INPUT: object = np.array([
-        (0, "this is a test"),
-        (1, "this is the second")
-    ])
+MSG_INPUT: list = [
+        (0, "this is a test", 2), # Message Location, Message, Translation Count (0, 1, 2)
+        (1, "this is the second", 2)
+    ]
 
 # Project Context
 DIR_PATH: str = os.path.join(os.path.dirname(__file__), "..", "api")
