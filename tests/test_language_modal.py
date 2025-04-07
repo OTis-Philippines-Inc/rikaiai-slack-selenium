e604d0625ab26f54bd31bea4c12f0d437ac8ca55
Victor <victorjosefcaro@gmail.com>
2025-04-07 09:55:51 +0800

    add validation for translation in language modal test

--- tests/test_language_modal.py
+++ tests/test_language_modal.py
@@ -6,6 +6,7 @@ from seleniumbase import BaseCase
 from actions.slackCommands.language_modal import LanguageModal
 from actions.loginPage.login import Login
 from utilities.playback import Playback
+from selenium.webdriver.common.by import By
 
 
 class LanguageModalTest(BaseCase):
@@ -29,5 +30,25 @@ class LanguageModalTest(BaseCase):
         self.click('button[data-qa="wizard_modal_next"]')
         self.wait(5)
 
+        modal.enter_command(self, "안녕하세요")
+        self.wait(2)
+
+        reply_bars = self.find_elements('div[data-qa="reply_bar"]')        
+        most_recent_reply = reply_bars[-1]
+        most_recent_reply.click()
+        self.wait(2)
+
+        self.wait_for_element_present('div[data-qa="message_content"]')
+        message_blocks = self.find_elements('div[data-qa="message_content"]')    
+        most_recent_message = message_blocks[-1]
+        
+        try:
+            most_recent_message.find_element(By.CSS_SELECTOR, 'img[alt=":cn:"]')
+            is_chinese = True
+        except:
+            is_chinese = False
+        
+        assert is_chinese, "Translation is not in Chinese (no CN flag found)"
+        
         self.record.stop_and_save_recording()
         self.driver.close()
