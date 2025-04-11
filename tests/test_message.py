02ef923550524122b97551434f52af5e24d48c60
Isiah Jordan Dimaunahan <dimaunahanisiahjordan@gmail.com>
2025-04-11 19:24:23 +0800

    feat: test language config

--- tests/test_message.py
+++ tests/test_message.py
@@ -6,6 +6,7 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "a
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "actions", "loginPage")))
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "utilities")))
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "config")))
+from translate import SlackConfig
 from settings import USER, SITE, EMAIL, CHANNEL, MSG_INPUT
 from login import Login
 from message import Message
@@ -27,17 +28,24 @@ class MessagePage(BaseCase):
         
         msg_handle: object = Message(self)
         bill_handle: object = Billing(self, msg_handle)
+        translate_handle: object = SlackConfig(self, msg_handle)
         for channel in CHANNEL:
             points: float = 0 # Monitor difference in score
             #points = bill_handle.get_channel_bill(channel)
-            for index, text, translation in MSG_INPUT:
-                print(index, text)
+            for index, text, translation, source, target, is_translated in MSG_INPUT:
+                # Set configurations
+                translate_handle.set_source(source)
+                translate_handle.set_target(target)
+                translate_handle.set_language(channel, is_translated)
+
                 msg_handle.update_message(text, USER, index, channel)
                 self.wait(3)
 
                 replies: list = msg_handle.get_reply(USER, "RikaiAI",index, channel)
                 print(replies)
                 msg_handle.config_language(channel)
+                
+                # Set translate
 
             self.assert_equal(2, 2)
 
